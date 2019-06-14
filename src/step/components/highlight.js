d149b7a98eceaa7b4c76485ad83efa3e0af4146b
Eugene Trounev <eugene.trounev@likalo.com>
2019-06-14 09:13:54 -0400

    Highlght should style diferently when there is action

--- src/step/components/highlight.js
+++ src/step/components/highlight.js
@@ -1,6 +1,6 @@
 import u from "umbrellajs";
 import { Icon } from "./icon";
 export const Highlight = ({ actiontarget }) =>
-    u(`<div class="guided-tour-step-highlight">
+    u(`<div class="guided-tour-step-highlight ${actiontarget ? "guided-tour-step-highlight-active" : ""}">
         ${actiontarget ? Icon({ name: "hand", className: "guided-tour-step-button-action" }) : ""}
     </div>`);
